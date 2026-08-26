a1c1239b909c8bc96f5ece15632c78aa670ce151
gunner madsen <gunnermadsen@capitonic.com>
2026-08-26 10:51:10 -0500

    feat(ingester): add focused health observability

--- common/scripts/prometheus-provisioning-entrypoint.sh
+++ common/scripts/prometheus-provisioning-entrypoint.sh
@@ -74,6 +74,11 @@ scrape_configs:
     static_configs:
       - targets:
           - polymarket-bot:8097
+  - job_name: market-data-ingester
+    metrics_path: /prometheus/metrics
+    static_configs:
+      - targets:
+          - market-data-ingester:8098
 EOF
 
 exec /bin/prometheus \
